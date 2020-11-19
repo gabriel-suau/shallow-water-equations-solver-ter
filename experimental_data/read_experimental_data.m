a89ab5220c18986ef6efb4d5fe99282828938236
Gabriel Suau <gabrielsuau2@gmail.com>
2020-11-19 14:19:38 +0100

    [EXP] Rename the octave script.

--- experimental_data/read_experimental_data.m
+++ experimental_data/read_experimental_data.m
@@ -1,27 +1,31 @@
 clear
 
+% Load experimental data from the .mat files.
 topo = load("topo_data_17OCT.mat");
 PT1 = load("data_PT1.mat");
 PT2 = load("data_PT2.mat");
 PT35 = load("data_sync_PT3-5.mat");
 
+% Get time vector.
 PT1.time = PT1.time - 738081;
 PT2.time = PT2.time - 738081;
 PT35.time = PT35.time - 738081;
 
+% Plot the topography and the position of the sensors.
 figure(1)
 plot(topo.x,topo.z)
 hold on
-plot(0,-1.19, "*")
+plot(0,-1.19, "*")    #PT1
 hold on
-plot(34.83,0.07, "*")
+plot(34.83,0.07, "*") #PT2
 hold on
-plot(52.88,0.74, "*")
+plot(52.88,0.74, "*") #PT3
 hold on
-plot(57.90,0.83, "*")
+plot(57.90,0.83, "*") #PT4
 hold on
-plot(62.08,0.96, "*")
+plot(62.08,0.96, "*") #PT5
 
+% Plot the height of water as a function of time for each sensor.
 figure(2)
 plot(PT1.time, PT1.h_hyd)
 hold on

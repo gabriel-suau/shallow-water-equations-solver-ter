9d8a1cf205db3f93642dc3d5272d673654d41383
Gabriel Suau <gabrielsuau2@gmail.com>
2020-11-20 17:47:59 +0100

    [TOOLS] Fix an error.

--- code/format_code.sh
+++ code/format_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Format the code using Artistic Style.
+# Format the code using clang-format.
 
 echo "Formatting your code with clang-format..."
 clang-format -style=file -fallback-style=none src/*.h src/*.cpp

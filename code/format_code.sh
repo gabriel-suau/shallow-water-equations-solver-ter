26f07de4012f0db1a1251b63f85da59fe12d2fa5
Gabriel Suau <gabrielsuau2@gmail.com>
2020-11-30 17:54:39 +0100

    [TOOLS] Modify the formatting tool.

--- code/format_code.sh
+++ code/format_code.sh
@@ -3,4 +3,4 @@
 # Format the code using clang-format.
 
 echo "Formatting your code with clang-format..."
-clang-format -style=file -fallback-style=none src/*.h src/*.cpp
+clang-format -style=file -fallback-style=none *.h *.cpp
